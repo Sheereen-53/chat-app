e6ff70f12ad28640180a0ffdf778ac1067bcf9bf
Sheereen <f20180697@goa.bits-pilani.ac.in>
2022-09-11 12:22:05 +0530

    Register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import Add from "../img/addAvatar.png"
 
 export const Register = () => {
   return (
-    <div>Register</div>
+    <div className="formContainer">
+        <div className="formWrapper">
+            <span className="logo">Quick Chat</span>
+            <span className="title">Register</span>
+            <form action="">
+                <input type="text" placeholder='display name'/>
+                <input type="email"  placeholder='email'/>
+                <input type="password" placeholder='password'/>
+                <input style = {{display:"none"}} type="file" id="file"/>
+                <label htmlFor="file">
+                    <img src={Add} alt="" />
+                    <span>Add an avatar</span>
+                </label>
+                <button>Sign up</button>
+            </form>
+            <p>Have an account? Login</p>
+        </div>
+    </div>
   )
 }
